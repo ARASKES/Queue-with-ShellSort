0e40adaf5b9d75fb0671430cbb56c7783d56a2ee
Andrey Ryzhak <andrey.ryzhak307@gmail.com>
2020-10-17 01:17:47 +0300

    Project uploaded

---
+++ QueueShellAlgorythm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace QueueShellAlgorythm
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random(); //  Создадим объект класса Random
+      Stopwatch stopwatch = new Stopwatch();  //  Создадим объект класса Stopwatch для замера времени
+
+      Queue queue;  //  Создадим экземпляр очереди
+
+      int[] quantitySelection = {
+        500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 20000, 50000, 75000, 100000, 250000
+      };  //  Создадим выборку размеров очереди для каждого из экспериментов
+
+      long ops = 0; //  Создадим переменную для записи количества операций
+
+      for (int sessionNumber = 1; sessionNumber <= quantitySelection.Length; sessionNumber++) //  Производим 10 экспериментов
+      {
+        queue = new Queue();  //  Создаем очередь, имеющую размер из выборки
+
+
+        for (int i = 0; i < quantitySelection[sessionNumber - 1]; i++)
+        {
+          queue.Enqueue(random.Next(1, 1000));  //  Заполняем очередь случайными числами от 1 до 1000
+        }
+
+        stopwatch.Start();
+        queue.ShellSort();  //  Вызываем метод сортировки очереди
+        stopwatch.Stop();
+        Console.WriteLine($"Sorting session number: {sessionNumber}\tNumbers sorted: {quantitySelection[sessionNumber - 1]}\tSorting time: {stopwatch.ElapsedMilliseconds} ms\tNumber of operations: {ops}\n");
+        stopwatch.Reset();
+        //Console.WriteLine();
+        //queue.PrintQueue();
+      }
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ QueueShellAlgorythm/Queue/Queue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class Queue
+{
+  int[] elements; //  Массив в качестве "ядра" очереди
+
+  public Queue()
+  {
+    elements = new int[] { };
+  }
+
+  public int Count
+  {
+    get
+    {
+      return elements.Length;
+    }
+  }
+
+  public bool isEmpty
+  {
+    get
+    {
+      return elements.Length == 0;
+    }
+  }
+
+  public void Enqueue(int value)
+  {
+    int[] cacheArray = new int[elements.Length + 1];
+
+    for (int i = 0; i < elements.Length; i++)
+    {
+      cacheArray[i] = elements[i];
+    }
+    cacheArray[elements.Length] = value;
+
+    elements = cacheArray;
+  }
+
+  public int Dequeue()
+  {
+    int elementToDelete = elements[0];
+
+    int[] cacheArray = new int[elements.Length - 1];
+
+    for (int i = 0; i < cacheArray.Length; i++)
+    {
+      cacheArray[i] = elements[i + 1];
+    }
+
+    elements = cacheArray;
+
+    return elementToDelete;
+  }
+
+  public int Peek()
+  {
+    return elements[0];
+  }
+
+  public void ShellSort()
+  {
+    //  Регулировка шага (стандартным методом Шелла, начиная с половины размера)
+    for (int step = elements.Length / 2; step > 0; step /= 2)
+    {
+      //  Перебор элементов, начиная с элемента, индекс которого равен шагу
+      for (int i = step; i < elements.Length; i++)
+      {
+        //  Перестановка элементов по возрастанию внутри участка между j-тым и i-тым элементами, до элемента с индексом i
+        for (int j = i - step; j >= 0 && elements[j] < elements[j + step]; j -= step)
+        {
+          int temp = elements[j];
+          elements[j] = elements[j + step];
+          elements[j + step] = temp;
+        }
+      }
+    }
+  }
+
+  public void PrintQueue()
+  {
+    if (elements.Length != 0)
+    {
+      foreach (int element in elements)
+      {
+        Console.Write($"{element} ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
